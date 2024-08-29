518685c3ee89038a59bee6d899e479568e5dc597
smpark <smpark7723@gmail.com>
2024-08-30 00:53:52 +0900

    Add : middleware 설정

---
+++ apps/smpark-space/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from 'next/server';
+
+import { authMiddleware } from '@/shared/api';
+
+export async function middleware(req: NextRequest) {
+  // let response: NextResponse;
+
+  // 인증 미들웨어 적용
+  const response = await authMiddleware(req);
+
+  // 인증이 성공하면 (response.ok가 true이면) nonce 미들웨어 적용
+  // if (response.ok) {
+  //   response = nonceMiddleware(req);
+  // }
+
+  return response;
+}
+
+// 특정 경로에 대해 미들웨어 적용
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
