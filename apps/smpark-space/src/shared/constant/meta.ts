69eb0e7b3896de9e56a9c29eebf8526118f4ccd6
smpark <smpark7723@gmail.com>
2024-08-30 01:04:52 +0900

    Add : 공유 상수 추가

---
+++ apps/smpark-space/src/shared/constant/meta.ts
@@ -0,0 +1,18 @@
+export const METADATA = {
+  DEFAULT: {
+    title: "smpark's space",
+    description: 'smpark의 개인 웹페이지 입니다.',
+    keywords: ['웹개발', 'JavaScript', 'React', 'Next.js'],
+    url: '/imgs/smpark2.webp',
+  },
+  BLOG: {
+    title: '블로그 | 내 멋진 웹사이트',
+    description: '최신 기술 트렌드와 팁을 제공하는 블로그',
+    keywords: ['블로그', '기술', '프로그래밍', '팁'],
+  },
+  ABOUT: {
+    title: '소개 | 내 멋진 웹사이트',
+    description: '우리 팀과 미션에 대해 알아보세요',
+    keywords: ['소개', '팀', '미션', '비전'],
+  },
+};
---
+++ apps/smpark-space/src/shared/constant/ui.ts
@@ -0,0 +1,6 @@
+export const URIS = {
+  loading: {
+    src: '/imgs/loading.webp',
+    alt: '로딩중 표시',
+  },
+};
