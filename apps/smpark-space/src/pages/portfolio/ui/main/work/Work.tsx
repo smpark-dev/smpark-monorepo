6daef45bacfd7d6969269737f15b748a4ce82ac7
smpark <smpark7723@gmail.com>
2024-09-11 15:59:21 +0900

    Add : work components 추가

--- apps/smpark-space/src/pages/portfolio/ui/main/work/Work.tsx
+++ apps/smpark-space/src/pages/portfolio/ui/main/work/Work.tsx
@@ -1,38 +1,18 @@
 import { WORK } from '@/pages/portfolio/constants';
-import { ToggleArrow } from '@/shared/ui';
 
-import { Project } from './Project';
 import { WorkExperience } from './WorkExperience';
 
 export const Work = () => (
-  <section id='work' className='portfolioSection mt-24 font-josefin'>
+  <section id='work' className='portfolioSection mt-24 font-noto'>
     <div className='portfolioTitleContainer'>
       <h1 className='text-5xl font-josefin'>{WORK.TITLE}</h1>
     </div>
-    <p className='mt-5 mb-24 text-base text-line font-noto'>{WORK.DESCRIPTION}</p>
+    <h2 className='mt-8 mb-[25px] text-xl font-josefin'>{WORK.SUBTITLE}</h2>
 
-    <div className='p-6 rounded-lg mb-4 bg-transparency-primary'>
-      {/* {WORK.EXPERIENCE.map((PROJECT) => (
-        <div key={PROJECT.TITLE}>
-
-        </div>
-      ))} */}
+    <div className='py-3 px-6 rounded-lg mb-4 bg-transparency-primary flex flex-wrap'>
+      {WORK.EXPERIENCE.map((exp) => (
+        <WorkExperience key={exp.TITLE} experience={exp} />
+      ))}
     </div>
-    {/* <ToggleArrow title='Project(2024)' toggleDefault>
-      <div className='flex flex-wrap justify-around'>
-        <h2 className='w-full mt-5 mb-11 text-3xl'>{WORK.portfolio_title}</h2>
-        {WORK.portfolio_list.map((item) => (
-          <Project key={item.id} project={item} />
-        ))}
-      </div>
-    </ToggleArrow>
-
-    <ToggleArrow title='Work Experience' toggleDefault>
-      <div className='mt-5'>
-        {WORK.work_list.map((item) => (
-          <WorkExperience key={item.id} item={item} />
-        ))}
-      </div>
-    </ToggleArrow> */}
   </section>
 );
