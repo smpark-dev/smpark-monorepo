2dc7d22955d592865f15888ba2ee148a3de87fab
smpark <smpark7723@gmail.com>
2024-12-03 17:32:07 +0900

    Add : smpark-space login loading

--- apps/smpark-space/src/features/auth/ui/LoginButtons.tsx
+++ apps/smpark-space/src/features/auth/ui/LoginButtons.tsx
@@ -1,19 +1,32 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
+import { useState } from 'react';
 
-import { Button } from '@/shared/ui';
+import { Button, Loading } from '@/shared/ui';
 
 const loginProvider = ['Google', 'Github', 'Smpark', 'Guest'];
 
 export const LoginButtons = () => {
-  return (
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingProvider, setLoadingProvider] = useState('');
+
+  const handleSignIn = async (provider: string) => {
+    setIsLoading(true);
+    setLoadingProvider(provider);
+    await signIn(provider.toLowerCase());
+  };
+
+  return isLoading ? (
+    <Loading width={269} height={269} className='mx-auto' />
+  ) : (
     <ul className='text-center m-[15px]'>
       {loginProvider.map((provider) => (
         <li className='my-[15px] mx-auto max-w-[300px]' key={provider}>
           <Button
             className='hover:bg-secondary hover:border-[1px] hover:border-solid hover:border-primary text-lg select-none w-full text-center mx-auto p-[13px] border-[1px] border-solid border-primary rounded-[30px] cursor-pointer'
-            onClick={() => signIn(provider.toLocaleLowerCase())}
+            onClick={() => handleSignIn(provider)}
+            disabled={isLoading && loadingProvider !== provider}
           >
             {provider}
           </Button>
